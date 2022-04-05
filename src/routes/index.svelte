2c7047e3025e364dd536171578dec894a815defa
Labib <lstriker1268@gmail.com>
2022-04-05 19:43:55 +0000

    Made letters

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,9 @@
 <script lang="ts">
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+    const letters = alphabet.split("");
 </script>
+<button class="border p-4 b-blue-500 rounded-md"> </button>
+{#each letters as letter}
+    {letter}
+{/each}
